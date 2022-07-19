79e3af336446d1a649b49db80d86e8a90646067e
Allen Mordukhaev <allenmor93@gmail.com>
2022-07-19 12:51:55 -0400

    commit tree 8acbb4cd124af25ddf98ba7f0c9f7ebcc4b6eef6

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(arr) {
+    return arr.slice(0, 2);
+}
+
+const returnLastTwoDrivers =  function(arr) {
+    return arr.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(a) {
+    return function(b) {
+        return a * b
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function(a, b) {
+    return b(a);
+}
